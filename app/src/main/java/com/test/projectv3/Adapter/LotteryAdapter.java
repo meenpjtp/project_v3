eab7aade0a61e272f34af839a970ceca8a4dc1fb
5710451398 <pinjuta.t@ku.th>
2018-01-27 01:52:50 +0700

    add database

---
+++ app/src/main/java/com/test/projectv3/Adapter/LotteryAdapter.java
@@ -0,0 +1,53 @@
+package com.test.projectv3.Adapter;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.test.projectv3.Model.LotteryModel;
+
+import java.util.List;
+
+public class LotteryAdapter extends RecyclerView.Adapter<LotteryAdapter.ViewHolder> {
+
+    public List<LotteryModel> data;
+
+    public LotteryAdapter(){}
+    public void setData(List<LotteryModel> data){
+        this.data = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View wordView = inflater.inflate(android.support.v4.R.layout.item_lottery, parent, false);
+        ViewHolder viewHolder = new ViewHolder(wordView, context);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(LotteryAdapter.ViewHolder holder, int position) {
+        LotteryModel lotteryModel = data.get(position);
+        holder.lotteryText.setText(lotteryModel.getLottery_num());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public Context context;
+        public TextView lotteryText;
+        public ViewHolder(View itemView, final Context context){
+            super(itemView);
+            this.context = context;
+            lotteryText = (TextView) findViewById(R.id.textView);
+        }
+    }
+}
--- app/src/main/java/com/test/projectv3/Lotteries/LotteryDecember_30_2560.java
+++ app/src/main/java/com/test/projectv3/Lotteries/LotteryDecember_30_2560.java
@@ -2,10 +2,14 @@ package com.test.projectv3.Lotteries;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.test.projectv3.R;
 
 public class LotteryDecember_30_2560 extends AppCompatActivity {
+
+    private RecyclerView rvLottery;
+    private LotteryAdater adater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
