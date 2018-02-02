2139b90597c071ce6c8b3cada8453e62fcef52b6
5710451398 <pinjuta.t@ku.th>
2018-02-03 00:22:39 +0700

    lottery database update

--- app/src/main/java/com/test/projectv3/LotteryActivities/LotteryDecember_16_2560.java
+++ app/src/main/java/com/test/projectv3/LotteryActivities/LotteryDecember_16_2560.java
@@ -7,9 +7,13 @@ import com.test.projectv3.R;
 
 public class LotteryDecember_16_2560 extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lottery_december_16_2560);
+
+//        final ImageView imageView = (ImageView)findViewById(R.id.imageView);
+//        imageView.setImageResource(R.drawable.lots30_12_60);
     }
 }
